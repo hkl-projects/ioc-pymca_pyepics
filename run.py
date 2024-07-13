9b6abe73827304bd785353520386bf9ebdf90b8e
Alex Baekey <alexanderbaekey@gmail.com>
2024-07-13 09:48:58 -0400

    added dependencies to conda env, working on exposing fit values

--- run.py
+++ run.py
@@ -4,8 +4,13 @@ import epics # pyepics
 #import time
 
 app = qt.QApplication([])
-wind = PyMcaMain()
+wind = PyMcaMain(debug=True)
 wind.sourceWidget.sourceSelector.openSource('/epics/support/pymca/data/exp798/Datafiles/HB3_exp0798_scan0090.dat')
+#ddict =
+#ddict["selection"] = {'cols': {'y': [1], 'x': [0]}} # line 1198 in PyMcaMain.py 
+#wind.dispatcherAddSelectionSlot(ddict) # line 517 in PyMcaMain.py
+#wind.dispatcherReplaceSelectionSlot(ddict) # line 687 in PyMcaMain.pyi
+print(wind.getConfig())
 wind.show()
 #time.sleep(7.5)
 app.exec()
